ac3536811b381900c29ebc87e2fb266139001a5b
Wieland Hilker <Wieland@PlusPlus.One>
2026-05-14 14:24:56 +0200

    added typing.

--- src/tdptdpbrowser/Browser/__init__.py
+++ src/tdptdpbrowser/Browser/__init__.py
@@ -15,7 +15,10 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
   # This allows for importing Typing without having to import and instanciate the object an additional time.
   # Defining the typing as classes allows for te use of opex().asType which is a big improvement.
-    class Typing(containerCOMP):
+    from .extTdpBrowser import extTdpBrowser
+    class Typing(extTdpBrowser, containerCOMP):
+        class par( extTdpBrowser.par, containerCOMP._ContainerCOMPPars):
+            pass
         pass
 else:
     class Typing:
