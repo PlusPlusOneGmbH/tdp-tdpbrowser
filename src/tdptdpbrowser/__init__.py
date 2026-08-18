22a80c704a08a9ce3274d0a0d6acc7c37abaadfc
Wieland Hilker <Wieland@PlusPlus.One>
2026-08-18 12:54:07 +0200

    added setupscript.

--- src/tdptdpbrowser/__init__.py
+++ src/tdptdpbrowser/__init__.py
@@ -2,6 +2,15 @@ __minimum_td_version__ = "2025.32460"
 
 from . import Browser
 
+from td import COMP, root, baseCOMP
+
+
 _ToxFiles  = {
     "Browser" : Browser.ToxFile
 }
+
+def Setup(home_op:COMP = None):
+    browser = (home_op or root.op("utils") or root.create(baseCOMP, "utils")).loadTox( Browser.ToxFile )
+    browser.par.externaltox.expr = f"mod.tdptdpbrowser.Browser.ToxFile"
+    browser.par.enableexternaltox.val = True
+    browser.par.enableexternaltoxpulse.pulse()
